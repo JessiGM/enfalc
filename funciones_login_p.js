67e01eef5732c800f04a210fd7694419d65c9fab
JessicaGaviria <yeka2040tr@hotmail.com>
2023-11-18 10:43:23 -0500

    Archivos de enfalc

---
+++ funciones_login_p.js
@@ -0,0 +1,21 @@
+function loguear() {
+    let email=document.getElementById("email").value;
+    let user=document.getElementById("usuario").value;
+    let pass=document.getElementById("password").value;
+    let confirm=document.getElementById("confirm_password").value;
+
+    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	let passRegex = /^[^\s@]+$/;
+
+    if(email !=="" && user !=="" && pass !=="" && confirm == pass) {
+        if (emailRegex.test(email) && passRegex.test(pass)) {
+            window.location="formulario_pac_reg.html";
+        }     
+    else {
+        alert("Por favor, ingresa un correo electrónico válido.");
+    }
+    }
+    else {
+        alert("Por favor ingresa los datos correctos.")
+    }
+}
